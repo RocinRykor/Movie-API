281d67569682d75bb2163b1bbf49d0c25267c259
Steven <Briggs.steven369@gmail.com>
2022-12-16 17:01:13 -0800

    added api endpoints for each project feature

--- index.js
+++ index.js
@@ -70,8 +70,117 @@ app.get('/documentation', (req, res) => {
 });
 
 //API Routes
+/*
+None of the routes below have their handler functions implemented
+Each simply returns a success string for the time being
+*/
+
+/**
+ * Fucntion -> READ all movies
+ * Param(s) -> None
+ * Request -> None
+ * Return -> JSON Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
 app.get('/movies', (req, res) => {
-    res.json(topMovie);
+    res.send('Successful GET request returning data on all the movies');
+});
+
+/**
+ * Fucntion -> READ data from single movie
+ * Param(s) -> title = Movie Title
+ * Request -> None
+ * Return -> JSON Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.get('/movies/:title', (req, res) => {
+    res.send('Successful GET request returning data on a single movie');
+});
+
+/**
+ * Fucntion -> READ data from single genre
+ * Param(s) -> genreTitle = Genre Title
+ * Request -> None
+ * Return -> JSON Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.get('/genre/:genreTitle', (req, res) => {
+    res.send('Successful GET request returning data on a single genre');
+});
+
+/**
+ * Fucntion -> READ data from single Director
+ * Param(s) -> directorName = Director Name
+ * Request -> None
+ * Return -> JSON Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.get('/directors/:directorName', (req, res) => {
+    res.send('Successful GET request returning data on a single director');
+});
+
+/**
+ * Fucntion -> CREATE data for a single User
+ * Param(s) -> None
+ * Request -> JSON Object
+ * Return -> JSON Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.post('/users', (req, res) => {
+    res.send('Successful POST request returning data on a new user');
+});
+
+/**
+ * Fucntion -> PUT data for a single User
+ * Param(s) -> id = User ID
+ * Request -> JSON Object
+ * Return -> JSON Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.put('/users/:id', (req, res) => {
+    res.send('Successful PUT request returning new data on the user');
+});
+
+/**
+ * Fucntion -> CREATE new movie entry for single user
+ * Param(s) -> id = User ID, movieTitle, Movie title
+ * Request -> None
+ * Return -> Text Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.post('/users/:id/:movieTitle', (req, res) => {
+    res.send('Successful POST request, added movie to users favorites');
+});
+
+/**
+ * Fucntion -> DELETE movie entry for single user
+ * Param(s) -> id = User ID
+ * Request -> None
+ * Return -> Text Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    res.send('Successful DELETE request, movie deleted from users favorites');
+});
+
+/**
+ * Fucntion -> DELETE single user
+ * Param(s) -> id = User ID
+ * Request -> None
+ * Return -> Text Object
+ *
+ * TODO - STATUS -> INCOMPLETE/NOT IMPLEMENTED
+ */
+app.delete('/users/:id', (req, res) => {
+    res.send('Successful DELETE request, User deleted');
 });
 
 //Error Handling
